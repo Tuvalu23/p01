6071b04a1f55d7b57c4ef66d8b55e470bfa9b121
Tuvalu23 <brudinski50@stuy.edu>
2024-11-28 00:36:46 -0500

    home page

--- app/__init__.py
+++ app/__init__.py
@@ -7,15 +7,24 @@ Time Spent: 0.5
 '''
 
 # imports
-from flask import Flask
-from .config import Config
+import os, sys
+
+# adding config.py to search path
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+from flask import Flask, render_template
+from config import Config
 
 # flask app initializing
 app = Flask(__name__)
 app.config.from_object(Config)
 
 # routes
-from . import routes
+@app.route('/') # home page route
+def home():
+    #maps api key
+    google_maps_api_key = app.config['GOOGLE_MAPS_API_KEY']
+    return render_template('home.html', google_maps_api_key=google_maps_api_key)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- app/routes.py
+++
@@ -1,7 +0,0 @@
-'''
-Team Bareustoph: Ben Rudinski, Tiffany Yang, Endrit Idrizi, Tim Ng
-SoftDev
-P01: ArRESTed Development - Global Bites
-2024-11-27
-Time Spent: 0.5
-'''
