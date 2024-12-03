df70c7cae9faf5bfb679f136f4377c03dd95f94a
Tiffany Yang <121731114+Vianne-a1@users.noreply.github.com>
2024-12-03 16:38:13 +0000

    sagesre

--- setup_db.py
+++ setup_db.py
@@ -17,16 +17,45 @@ def setup_database():
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 username TEXT UNIQUE NOT NULL,
                 password_hash TEXT NOT NULL,
-                recents INTERGER NULL
+                join_date INTERGER NULL,
+                badges TEXT NOT NULL
+
             )
         ''')
         conn.execute('''
-            CREATE TABLE IF NOT EXISTS threads (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
+            CREATE TABLE IF NOT EXISTS Comments (
+                comment_id INTEGER PRIMARY KEY AUTOINCREMENT,
                 user_id INTEGER NOT NULL,
+                recipie_id INTERGER NOT NULL,
+                text TEXT NOT NULL,
+                comment_time INTERGER NULL
+            )
+        ''')
+        conn.execute('''
+            CREATE TABLE IF NOT EXISTS Recipies (
+                recipie_id INTEGER NOT NULL,
                 title TEXT NOT NULL,
-                content TEXT NOT NULL,
-                FOREIGN KEY (user_id) REFERENCES users (id)
+                ingredients TEXT NOT NULL,
+                instructions TEXT NOT NULL,
+                cuisine TEXT NULL,
+                image_url TEXT NULL
+            )
+        ''')
+        conn.execute('''
+            CREATE TABLE IF NOT EXISTS Badges (
+                badge_id INTEGER NULL,
+                badge_name TEXT NOT NULL,
+                description TEXT NOT NULL,
+                icon_url TEXT NOT NULL
+            )
+        ''')
+        conn.execute('''
+            CREATE TABLE IF NOT EXISTS RecentHistory(
+                history_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                user_id INTEGER NOT NULL,
+                recipie_id INTERGER NOT NULL,
+                interaction_type TEXT NOT NULL,
+                comment_time INTERGER NULL
             )
         ''')
     conn.close()
@@ -36,18 +65,20 @@ if __name__ == "__main__":
 
 
 
-# Users
+# Users -done
 # user_id (primary key)
 # username
 # password_hash
-# join_date
+# join_date (unix time stamp?)
 # badges
+
 # Comments
 # comment_id (primary key)
 # user_id (foreign key) recipe_id (foreign
 # key)
 # text
 # timestamp
+
 # Recipes
 # recipe_id (primary
 # key)
@@ -56,6 +87,7 @@ if __name__ == "__main__":
 # instructions
 # cuisine
 # image_url
+
 # Badges
 # badge_id (primary key) badge_name
 # description icon_url
